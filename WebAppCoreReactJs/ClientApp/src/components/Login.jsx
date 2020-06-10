c2dd2c373f73aee1f929674351d3444a4303d798
Fúlvio <fulviocanducci@hotmail.com>
2020-06-10 12:39:42 -0300

    Add project files.

---
+++ WebAppCoreReactJs/ClientApp/src/components/Login.jsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+function Login() {
+    return (
+        <div>
+            Login do Usuário
+        </div>);
+}
+
+export default Login;
---
+++ WebAppCoreReactJs/ClientApp/src/components/index.js
@@ -0,0 +1,15 @@
+﻿import Counter from './Counter';
+import FetchData from './FetchData';
+import Home from './Home';
+import Layout from './Layout';
+import NavMenu from './NavMenu';
+import Login from './Login';
+
+export {
+    Counter,
+    FetchData,
+    Home,
+    Layout,
+    NavMenu,
+    Login
+}
